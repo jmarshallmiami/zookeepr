1d37c135b151079747f71d850cf9c35beb1260f0
Jules <j.marshallmiami@yahoo.com>
2022-06-06 00:20:17 -0500

    initial server connection and query functions created

--- server.js
+++ server.js
@@ -44,12 +44,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
-    let results = animals;
+    let result = animals;
     if (req.query) {
-        results = filterByQuery(req.query, results);
+        result = filterByQuery(req.query, result);
     }
-    res.json(results);
+    if(result){
+    res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    res.json(result);
 });
 
 
